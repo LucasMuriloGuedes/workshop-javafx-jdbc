b867e2b6944e4b8982669d68857555175d9dcda4
LUCAS MURILO GUEDES OLIVEIRA <lucasmurilo.guedes@gmail.com>
2021-04-12 15:55:53 -0400

    Enviando atividades do curso de java para o github

---
+++ worshop-javafx-jdbc/src/worshop/javafx/jdbc/WorshopJavafxJdbc.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worshop.javafx.jdbc;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lucas Murilo
+ */
+public class WorshopJavafxJdbc extends Application {
+    
+@Override
+public void start(Stage primaryStage) { 
+    try { 
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); 
+        ScrollPane scrollPane = loader.load(); 
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        
+        Scene mainScene = new Scene(scrollPane); 
+        primaryStage.setScene(mainScene); 
+        primaryStage.setTitle("Sample JavaFX application"); 
+        primaryStage.show(); 
+     } catch (IOException e) { 
+        e.printStackTrace(); 
+     } 
+    } 
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
